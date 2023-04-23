0950f6d0b1ee9fe685d34c95fbf16a0422d49fe2
Geice Sousa Pinho <geicesousa@outlook.com>
2023-04-23 13:24:22 -0300

    detalhes de css a/hover

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,13 @@
-// oq eu quero?
-// em contato, 
-
-// ao clicar no botão enviar
-//     se os campos nome ou email não estiverem preenchidos
-//     pedir para preencher
-// se o campo não preenchido for mensagem/textarea 
-//  pedir para escrever uma mensagem
-
-
-function entrarEmContato(){
+document.getElementById('botao_contato').addEventListener("click", function entrarEmContato(){
       
     if(document.getElementById('nome').value === ""
         || document.getElementById('email').value === ""
             || document.getElementById('mensagem').value === ""){
-        alert("Por favor, preencha os campos obrigatórios.")
+       getElementById('contato--formulario').reportValidity()
     }   
     else{
-        alert("Seu contato foi realizado com sucesso. Em breve entrarei em contato. Até mais!")
+        alert("Seu contato foi realizado com sucesso. Entrarei em contato em breve. Até mais!")
 
     }
-}
-
-document.getElementById('botao_contato').addEventListener("click", entrarEmContato)
+})
 
